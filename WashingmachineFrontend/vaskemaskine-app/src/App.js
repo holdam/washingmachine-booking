9219555b8e5fbbb4a36b429e7f99d13674ef2555
Jens Holdam <jens@holdam.dk>
2016-10-28 23:29:44 +0200

    before routing

--- WashingmachineFrontend/vaskemaskine-app/src/App.js
+++ WashingmachineFrontend/vaskemaskine-app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CalendarAndUsage from './CalendarAndUsage/CalendarAndUsage';
+import strings from './strings'
+import { Router, Route, hashHistory } from 'react-router'
 
 class App extends Component {
     render() {
@@ -9,7 +11,7 @@ class App extends Component {
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Nordre Ringgade 108 vaskerumbooking</h2>
+                    <h2>{strings.frontpageTitle}</h2>
                 </div>
                 <CalendarAndUsage />
             </div>
---
+++ WashingmachineFrontend/vaskemaskine-app/src/strings.js
@@ -0,0 +1,5 @@
+var strings = {
+    frontpageTitle: 'Nordre Ringgade 108 vaskerumbooking'
+};
+
+export default strings;
