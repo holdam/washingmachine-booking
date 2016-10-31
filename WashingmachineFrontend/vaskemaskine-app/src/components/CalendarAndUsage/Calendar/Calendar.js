b4f7850d9a6dc41efcb30d28690b8a23fc1d565b
Jens Holdam <jens@holdam.dk>
2016-10-31 23:06:50 +0100

    refactor

--- WashingmachineFrontend/vaskemaskine-app/src/components/CalendarAndUsage/Calendar/Calendar.js
+++ WashingmachineFrontend/vaskemaskine-app/src/components/CalendarAndUsage/Calendar/Calendar.js
@@ -21,42 +21,41 @@ class Calendar extends React.Component {
 
 class Week extends React.Component {
     render() {
-        // Someone more smart than me should rewrite this shit, to avoid creating the same almost identical element 3 x 2 times.
         let daysOfWeek = this.props.week.days.map((date) => {
-            let keyForDate = `${date.year}-${date.month}-${date.day}`;
+            let keyForDate = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
 
             // Check if we're in the first week, since we will add weekday names
             if (this.props.week.weekOfCalendar === 1) {
-                let dayOfTheWeek = weekdayNames[new Date(date.year, date.month, date.day).getUTCDay()];
+                let dayOfTheWeek = weekdayNames[new Date(date.getFullYear(), date.getMonth(), date.getDate()).getUTCDay()];
 
                 // First week offmonth-days
-                if (this.props.currentlySelectedMonth !== date.month) {
-                    return <Day key={keyForDate} date={date} offMonthDay={true} onClick={this.props.onDayClick}>{`${dayOfTheWeek} ${date.day}`}</Day>
+                if (this.props.currentlySelectedMonth !== date.getMonth()) {
+                    return <Day key={keyForDate} date={date} offMonthDay={true} onClick={this.props.onDayClick}>{`${dayOfTheWeek} ${date.getDate()}`}</Day>
                 }
 
                 // First week, regular days
                 // Check if the first day of month, since we will add month rather than weekday
-                if (date.day === 1) {
-                    return <Day key={keyForDate} date={date} onClick={this.props.onDayClick}>{`${monthNamesShort[date.month]} ${date.day}`}</Day>
+                if (date.getDate() === 1) {
+                    return <Day key={keyForDate} date={date} onClick={this.props.onDayClick}>{`${monthNamesShort[date.getMonth()]} ${date.getDate()}`}</Day>
                 }
 
-                return <Day key={keyForDate} date={date} onClick={this.props.onDayClick}>{`${dayOfTheWeek} ${date.day}`}</Day>
+                return <Day key={keyForDate} date={date} onClick={this.props.onDayClick}>{`${dayOfTheWeek} ${date.getDate()}`}</Day>
             }
 
             // Not in first week
             // Check if we're in an off-month
-            if (this.props.currentlySelectedMonth !== date.month) {
-                if (date.day === 1) {
-                    return <Day key={keyForDate} date={date} offMonthDay={true} onClick={this.props.onDayClick}>{`${monthNamesShort[date.month]} ${date.day}`}</Day>
+            if (this.props.currentlySelectedMonth !== date.getMonth()) {
+                if (date.getDate() === 1) {
+                    return <Day key={keyForDate} date={date} offMonthDay={true} onClick={this.props.onDayClick}>{`${monthNamesShort[date.getMonth()]} ${date.getDate()}`}</Day>
                 }
-                return <Day key={keyForDate} date={date} offMonthDay={true} onClick={this.props.onDayClick}>{date.day}</Day>
+                return <Day key={keyForDate} date={date} offMonthDay={true} onClick={this.props.onDayClick}>{date.getDate()}</Day>
             }
 
             // Else we're in a regular month
-            if (date.day === 1) {
-                return <Day key={keyForDate} date={date} onClick={this.props.onDayClick}>{`${monthNamesShort[date.month]} ${date.day}`}</Day>
+            if (date.getDate() === 1) {
+                return <Day key={keyForDate} date={date} onClick={this.props.onDayClick}>{`${monthNamesShort[date.getMonth()]} ${date.getDate()}`}</Day>
             }
-            return <Day key={keyForDate} date={date} onClick={this.props.onDayClick}>{date.day}</Day>
+            return <Day key={keyForDate} date={date} onClick={this.props.onDayClick}>{date.getDate()}</Day>
         });
 
         return (
@@ -73,7 +72,7 @@ function Day(props) {
     if (props.offMonthDay === true) {
         classes += "off-month-day "
     }
-    if (props.date.year === today.getFullYear() && props.date.month === today.getMonth() && props.date.day === today.getDate()) {
+    if (props.date.getDate() === today.getFullYear() && props.date.getMonth() === today.getMonth() && props.date.getDate() === today.getDate()) {
         classes += "today "
     }
 
--- WashingmachineFrontend/vaskemaskine-app/src/components/CalendarAndUsage/CalendarAndUsage.js
+++ WashingmachineFrontend/vaskemaskine-app/src/components/CalendarAndUsage/CalendarAndUsage.js
@@ -4,14 +4,6 @@ import Usage from './Usage/Usage';
 import MonthPicker from './Monthpicker/Monthpicker';
 import './CalendarAndUsage.css';
 
-class DateRepresentation {
-    constructor(year, month, day) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
-    }
-}
-
 class WeekRepresentation {
     constructor(days, weekOfCalendar) {
         this.days = days;
@@ -23,7 +15,7 @@ class CalendarAndUsage extends React.Component {
     getWeeksOfMonth() {
         let weeks = [];
         let thisMonth = new Date(this.props.selectedMonth.getFullYear(), this.props.selectedMonth.getMonth() + 1, 0);
-        let lastMonth = new Date(thisMonth.getFullYear(), thisMonth.getMonth() - 1, 0);
+        let lastMonth = new Date(thisMonth.getFullYear(), thisMonth.getMonth(), 0);
 
         // Handling first week
         let firstDayOfWeekOfMonth = new Date(thisMonth.getFullYear(), thisMonth.getMonth(), 1).getUTCDay();
@@ -32,44 +24,46 @@ class CalendarAndUsage extends React.Component {
         // Days of previous month we want in the beginning of our week
         for (var weekday = 1; weekday <= firstDayOfWeekOfMonth; weekday++) {
             let theDate = lastMonth.getDate() - (firstDayOfWeekOfMonth - weekday);
-            firstWeek.days.push(new DateRepresentation(lastMonth.getFullYear(), lastMonth.getMonth(), theDate));
+            firstWeek.days.push(new Date(lastMonth.getFullYear(), lastMonth.getMonth(), theDate));
         }
 
         // Days of current month, fill out the week with current days of the month
         for (let i = 1; weekday <= 7; weekday++) {
-            firstWeek.days.push(new DateRepresentation(thisMonth.getFullYear(), thisMonth.getMonth(), i++));
+            firstWeek.days.push(new Date(thisMonth.getFullYear(), thisMonth.getMonth(), i++));
         }
 
         weeks.push(firstWeek);
 
         // Get next day of month after the first week and iterate until the end of the month
         let currentWeek = 0;
-        for (let currentDay = firstWeek.days[6].day + 1; currentDay <= thisMonth.getDate(); currentDay++) {
+        for (let currentDay = firstWeek.days[6].getDate() + 1; currentDay <= thisMonth.getDate(); currentDay++) {
             // First day of week
-            if ((currentDay - (firstWeek.days[6].day + 1)) % 7 === 0) {
+            if ((currentDay - (firstWeek.days[6].getDate() + 1)) % 7 === 0) {
                 currentWeek++;
-                weeks.push(new WeekRepresentation([new DateRepresentation(thisMonth.getFullYear(), thisMonth.getMonth(), currentDay)], currentWeek + 1));
+                weeks.push(new WeekRepresentation([new Date(thisMonth.getFullYear(), thisMonth.getMonth(), currentDay)], currentWeek + 1));
                 continue;
             }
-            weeks[currentWeek].days.push(new DateRepresentation(thisMonth.getFullYear(), thisMonth.getMonth(), currentDay));
+            weeks[currentWeek].days.push(new Date(thisMonth.getFullYear(), thisMonth.getMonth(), currentDay));
         }
 
         // Pad last week
-        let lastDayInsertedIntoCurrentWeek = weeks[currentWeek].days[weeks[currentWeek].days.length - 1].day;
+        let lastDayInsertedIntoCurrentWeek = weeks[currentWeek].days[weeks[currentWeek].days.length - 1].getDate();
         let lastWeekdayOfMonth = new Date(thisMonth.getFullYear(), thisMonth.getMonth(), lastDayInsertedIntoCurrentWeek).getUTCDay();
         let nextMonth = (thisMonth.getMonth() + 1) % 12;
         let nextYear = (nextMonth === 0) ? thisMonth.getFullYear() + 1 : thisMonth.getFullYear();
         for (var currentDayOfMonth = 1; lastWeekdayOfMonth + 1 < 7; lastWeekdayOfMonth++) {
-            weeks[currentWeek].days.push(new DateRepresentation(nextYear, nextMonth, currentDayOfMonth++));
+            weeks[currentWeek].days.push(new Date(nextYear, nextMonth, currentDayOfMonth++));
         }
 
         // Make sure we have 6 weeks
         if (weeks.length < 6) {
             weeks.push(new WeekRepresentation([], 6));
             for (let lastDayOfWeek = currentDayOfMonth + 7; currentDayOfMonth < lastDayOfWeek; currentDayOfMonth++) {
-                weeks[5].days.push(new DateRepresentation(nextYear, nextMonth, currentDayOfMonth));
+                weeks[5].days.push(new Date(nextYear, nextMonth, currentDayOfMonth));
             }
         }
+
+        console.log(weeks);
 
         return weeks;
     }
